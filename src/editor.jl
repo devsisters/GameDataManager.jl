bb84f87feb5ca02d5e8b8c682b8bb52a85545d54
김용희 <yongheekim@devsisters.com>
2019-05-10 15:24:18 +0900

    PartTime.xlsx NeedScore 계산기능 추가

--- src/editor.jl
+++ src/editor.jl
@@ -172,19 +172,32 @@ function editor_PartTime!(jwb)
     function get_dice(sheet)
         jwb[:Setting][1, sheet] |> x -> range(x["Min"], step = x["Step"], stop = x["Max"])
     end
-    # 데이터 불러오기
+
+    # TODO PartTime Group별로 테이블 분리
     ndices = jwb[:Setting][1, :Rounds] * jwb[:Setting][1, :MaxPipo]
     for s in [:BaseScore, :PerkBonusScore]
         x = dice_distribution(ndices, get_dice(s))
-        # TODO PartTime Group별로 테이블 분리
         df = DataFrame(Throw = 1:length(x))
         for k in keys(x[1])
             df[k] = broadcast(el -> el[k], x)
         end
         jwb[s] = df
     end
+
+    # NeedScore 입력해주기
+    jws = jwb[:Group]
+    for i in 1:size(jws, 1)
+        cum_prob = begin
+            throw = jws[i, :PipoCountRecommended] * jwb[:Setting][1, :Rounds]
+            a = jwb[:BaseScore][throw, :Weight]
+            broadcast(x -> sum(a[1:x]) / sum(a), eachindex(a))
+        end
+        target = broadcast(st -> findfirst(x -> x >= st, cum_prob), values(jws[i, :Standard]))
+        jws[i, :NeedScore] = jwb[:BaseScore][throw, :Outcome][target]
+    end
     jwb
 end
+
 
 
 """
