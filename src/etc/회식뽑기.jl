688a139897483cc4384fd5b01f3a24498c845605
김용희 <yongheekim@devsisters.com>
2019-06-05 13:43:20 +0900

    회식뽑기 개편

--- src/etc/회식뽑기.jl
+++ src/etc/회식뽑기.jl
@@ -6,33 +6,32 @@ function 마스인력현황()
          :아트 => ["신지영", "원밝음", "박윤서", "이재건", "원병일", "황민지"])
 end
 
-function 회식뽑기()
-    # 사람 섞기
-    전체 = 마스인력현황()
-    for el in 전체
-        shuffle!(el[2])
-    end
-
-    a = []
-    b = []
-    for i in 1:50
-        x = rand(keys(전체))
-        if length(전체[x]) > 0
-            if length(전체[x]) >= 2
-                push!(a, pop!(전체[x]))
-                push!(b, pop!(전체[x]))
-            else
-                length(a) > length(b) ? push!(b, pop!(전체[x])) : push!(a, pop!(전체[x]))
-            end
-        end
-    end
-    return a, b
-end
+# function 회식뽑기_직군섞기()
+#     # 사람 섞기
+#     전체 = 마스인력현황()
+#     for el in 전체
+#         shuffle!(el[2])
+#     end
+#
+#     a = []
+#     b = []
+#     for i in 1:50
+#         x = rand(keys(전체))
+#         if length(전체[x]) > 0
+#             if length(전체[x]) >= 2
+#                 push!(a, pop!(전체[x]))
+#                 push!(b, pop!(전체[x]))
+#             else
+#                 length(a) > length(b) ? push!(b, pop!(전체[x])) : push!(a, pop!(전체[x]))
+#             end
+#         end
+#     end
+#     return a, b
+# end
 
 function 회식뽑기_전체(열외자)
     전체 = vcat(collect(values(마스인력현황()))...)
     전체 = setdiff(전체, 열외자)
-
     shuffle!(전체)
 
     a = 전체[1:7]
@@ -40,19 +39,7 @@ function 회식뽑기_전체(열외자)
     return a, b
 end
 
-
-a, b = 회식뽑기_전체(["노찬우"])
-
-println("---회식 1조---")
-printstyled(a; color = :blink)
-println("\n\n---회식 2조---")
-printstyled(b; color = :yellow)
-
-
-
-
-
-function 히스토리고려해_회식뽑기(기획, 프로, 아트, his)
+function 히스토리고려해_회식뽑기(열외자, h)
     function count_history(team)
         x = Dict(1 =>0, 2 =>0)
         if !isempty(team)
@@ -62,46 +49,60 @@ function 히스토리고려해_회식뽑기(기획, 프로, 아트, his)
         return x
     end
 
-    과거조 = merge(Dict(zip(his[1], fill(1, length(his[1])))),
-                   Dict(zip(his[2], fill(2, length(his[2])))))
+    과거조 = merge(Dict(zip(h[1], fill(1, length(h[1])))),
+                   Dict(zip(h[2], fill(2, length(h[2])))))
 
-    [shuffle!(el) for el in (기획, 프로, 아트)]
-    전체 = Dict(:기획 => 기획, :프로 => 프로, :아트 => 아트)
+    전체 = vcat(회식뽑기_전체(열외자)...)
 
     a = []
     b = []
-    for i in 1:50
-        x = rand(keys(전체))
-        if length(전체[x]) > 0
-            if length(전체[x]) >= 2
-                me = pop!(전체[x])
-                my_history = get(과거조, me, 1)
-
-                # 나랑 hisotry 공유가 더적은 조에 넣음
-                if count_history(a)[my_history] < count_history(b)[my_history]
-                    push!(a, me)
-                    push!(b, pop!(전체[x]))
-                else
-                    push!(b, me)
-                    push!(a, pop!(전체[x]))
-                end
+    # 히스토리 공유가 한명이라도 적은 곳에 넣기
+    for me in 전체
+        me_before = get(과거조, me, 0)
+        if me_before > 0
+            if count_history(a)[me_before] < count_history(b)[me_before]
+                push!(a, me)
             else
-                length(a) > length(b) ? push!(b, pop!(전체[x])) : push!(a, pop!(전체[x]))
+                push!(b, me)
             end
+        end
+    end
+    if abs(length(a) - length(b)) > 1
+        if length(a) > length(b)
+            push!(b, pop!(a))
+        else
+            push!(a, pop!(b))
         end
     end
     return a, b
 end
 
+function 드라마틱하게_결과보여주기(group, color; delay = 0.65)
+    print("  ┗")
+    for el in group
+        printstyled(el; color = color)
+        last(group) != el && print(", ")
+        sleep(delay)
+    end
+    print("\n")
+end
+
 # 여기에 회식조 히스토리 기록하기
 history = Dict(:십구년일월 =>
     (["황민지", "신지영", "원밝음", "유주원", "김지인", "김대원", "박정수", "김상복"],
-    ["이재건", "박윤서", "원병일", "노찬우", "김용희", "신석영", "전정은"]))
+    ["이재건", "박윤서", "원병일", "노찬우", "김용희", "신석영", "전정은"]),
+    :십구년오월 =>
+    (["원밝음", "이재건", "김지인", "진정은", "김용희", "백승대"],
+    ["신지영", "박윤서", "신석영", "원병일", "박정수"])
+    )
 
+###########################################################
+## 실행 단
+#############################################################
+a, b = 히스토리고려해_회식뽑기(["백승대"], history[:십구년오월])
 
-a, b = 히스토리고려해_회식뽑기(기획, 프로, 아트, history[:십구년일월])
-
-println("---회식 1조---")
-printstyled(a; color = :blink)
-println("\n\n---회식 2조---")
-printstyled(b; color = :yellow)
+println("ㅁ---좌석 배정 1조---ㅁ")
+드라마틱하게_결과보여주기(a, :blink)
+println()
+println("ㅁ---좌석 배정 2조---ㅁ")
+드라마틱하게_결과보여주기(b, :yellow)
