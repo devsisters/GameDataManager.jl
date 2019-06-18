0b25bc0171ab4189eac08dd5ee8bf0b27ad10ea5
김용희 <yongheekim@devsisters.com>
2019-06-18 10:46:39 +0900

    이름 중복 안되게 뽑기

--- src/feature/village.jl
+++ src/feature/village.jl
@@ -95,8 +95,8 @@ function create_dummyaccount(amount; )
     end
 
     v = []
-    villagenames = getgamedata("NameGenerator", :WorldENG)[1, :Borough]
-    accountnames = ["인생이트롤", "단호박정식", "사기캐다", "사약왕드링킹",
+    villagenames = shuffle(getgamedata("NameGenerator", :WorldENG)[1, :Borough])
+    accountnames = shuffle(["인생이트롤", "단호박정식", "사기캐다", "사약왕드링킹",
     "엄마쟤흙먹어", "인간되기글른인간", "난폭한말광량이", "데마시아의귀감", "아프리카청춘이다",
     "벼랑위의당뇨", "돈들어손내놔", "이마로도끼까", "오뎅볶이데스까", "나름전설이다", "원할머니봐쌈", "캐리비안의해녀",
     "반만고양이", "이웃집또털어", "오십칠분고통정보", "집수리영역가형", "반지하의제왕", "이빨까기인형", "메뚜기3분요리", "천국의계란",
@@ -104,7 +104,10 @@ function create_dummyaccount(amount; )
     "보일러댁에아버님", "악의공룡둘리", "추적60인분", "라스트사물놀이", "시베리안허숙히", "백마탄환자", "환갑포청천", "벼락식혜",
     "운도형밴드", "아줌마가대왕", "고스톱바둑왕", "음주소년아톰", "하얀마음뷁구", "닭큐멘터리", "유치원일진", "무즙파워레인저",
     "농약먹구쿠우", "곰탕재료푸우", "오사마빈모뎀", "엎드려벌쳐", "노스트라단무지", "통키왕피구", "바람의점심", "스님백원만", "초록불고기",
-    "사담후시단", "태조샷건"]
+    "사담후시단", "태조샷건", "야구왕홍길동", "길위의꺼벙이", "메론맛다시다", "아리아나그란데말입니다", "나의라임쩌는나무",
+    "된장님원장찌개배달왔습니다","전이만갑오개혁","가위왕핑킹","조선왕조씰룩","머리좀감우성","재시켜알바니깐","GUESS레기",
+    "피구내죽겠네","닭은살걀","데스도트","연퀘소문","리치킹의분뇨","난잘생겨서트롤함","인하철도구로구","아버님께효도르",
+    "신석기골반게리안", "킬숑숑와드탁","정신줄절단기","매운오리새끼","사제님저도클릭좀","방구맛우유","돈도없는데술사래","폐인28호"])
 
     profile_pics = begin
         p = joinpath(GAMEPATH[:mars_repo], "unity/Assets/5_GameData/Images_BotProfile")
@@ -118,17 +121,16 @@ function create_dummyaccount(amount; )
         # Special
         segments["Special"] = foo(vill, "Special")
         # Shop
-        segments["Shop"] = foo(vill, "Shop", 1:8)
+        segments["Shop"] = vcat(broadcast(x -> foo(vill, "Shop", 1:8), 1:5)...)
         # Residence 5개씩
         segments["Residence"] = vcat(broadcast(x -> foo(vill, "Residence", 1:8), 1:3)...)
         # SandBox
         segments["Sandbox"] = foo(vill, "Sandbox")
 
-
         push!(v, OrderedDict(
-                "Mid" => "dummy_ac_$i",
-                "VillageName" => rand(villagenames),
-                "ProfilePic" => rand(accountnames),
+                "Mid" => accountnames[i],
+                "VillageName" => villagenames[i],
+                "ProfilePic" => rand(profile_pics),
                 "TotalDevPoint" => 1,
                 "UserResources" => Dict("Coin" => 1, "Fuel" => 1),
                 "Segments" => segments))
