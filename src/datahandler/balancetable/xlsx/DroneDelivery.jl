6ac11f9f855c15f1453da0231d5e4b6dc1859df2
김용희 <yongheekim@devsisters.com>
2019-08-14 10:01:51 +0900

    fix parser

--- src/datahandler/balancetable/xlsx/DroneDelivery.jl
+++ src/datahandler/balancetable/xlsx/DroneDelivery.jl
@@ -2,15 +2,15 @@
 
 function parser_DroneDelivery(jwb::JSONWorkbook)
     d = OrderedDict{Symbol, Dict}()
-    for row in eachrow(jwb[:Group][:])
+    for row in eachrow(df(jwb[:Group]))
         key = Symbol(row[:GroupKey])
         d[key] = Dict{Symbol, Any}(:RewardKey => row[:RewardKey])
     end
-    for gdf in groupby(jwb[:Order][:], :GroupKey)
+    for gdf in groupby(df(jwb[:Order]), :GroupKey)
         key = Symbol(gdf[1, :GroupKey])
         v =  map((dec, item) -> NamedTuple{(:Desc, :Items)}((dec, item)),
-                                            gdf[Symbol("\$Desc")], gdf[:Items])
-        d[key][:Order] = Dict{Int32, Any}(zip(gdf[:Key], v))
+                                            gdf[!, Symbol("\$Desc")], gdf[!, :Items])
+        d[key][:Order] = Dict{Int32, Any}(zip(gdf[!, :Key], v))
     end
     return d
 end
