e07680fcc87e2fe68dbc77ccb5a7b4ec7a5395d8
김용희 <yongheekim@devsisters.com>
2019-08-29 16:00:21 +0900

    기본 재화 추가 삭제 테스트 추가

--- test/user.jl
+++ test/user.jl
@@ -7,25 +7,25 @@ itemtable = get(BalanceTable, "ItemTable")
 @testset "Currency 추가 삭제" begin
     u = User()
     creationitem = get(Dict, ("GeneralSetting", 2))[1]
-    @test getitem(u, CON) == creationitem["AddCoin"] * CON
-    @test remove!(u, creationitem["AddCoin"] * CON)
+    @test getitem(u, COIN) == creationitem["AddCoin"] * COIN
+    @test remove!(u, creationitem["AddCoin"] * COIN)
 
     @test getitem(u, CRY) == creationitem["AddCrystal"] * CRY
     @test remove!(u, creationitem["AddCrystal"] * CRY)
 
-    for C in (CRY, CON, ENERGYMIX, SITECLEANER, SPACEDROPTICKET, DEVELIPMENTPOINT)
+    for C in (CRY, COIN, ENERGYMIX, SITECLEANER, SPACEDROPTICKET, DEVELIPMENTPOINT)
         @test add!(u, 1000C)
         @test has(u, 1000C)
         @test remove!(u, 1000C)
         @test getitem(u, C) == zero(C)
-        @test remove!(u, C) == false
+        @test remove!(u, 10C) == false
     end
 end
 
 @testset "NormalItem 추가 삭제" begin
     ref = get(DataFrame, itemtable, "Normal")
 
-    u = User()
+    u = User(); remove!(u, StackItem(7001)) 
     for k in ref[!, :Key]
         @test add!(u, StackItem(k, 2))
         @test has(u, StackItem(k, 2))
@@ -36,7 +36,7 @@ end
 @testset "BuildingSeed 추가 삭제" begin
     ref = get(DataFrame, itemtable, "BuildingSeed")
 
-    u = User()
+    u = User(); remove!(u, StackItem(5001)) 
     for k in ref[!, :Key]
         @test add!(u, StackItem(k, 11))
         @test has(u, StackItem(k, 11))
@@ -54,10 +54,44 @@ end
     @test add!(u, allitem)
     @test has(u, allitem)
     @test remove!(u, allitem)
-
 end
 
 @testset "EnergyMix 구매" begin
+    u = User()
+    @time getitem(u, ENERGYMIX) == zero(ENERGYMIX)
 
+    ref = get(DataFrame, ("EnergyMix", "Price"))
+    # TODO price가 제대로 계산됐는지 확인하는 함수 추가
+    for i in 1:50
+        cost = price(u, ENERGYMIX)
+        @test buy!(u, ENERGYMIX) == false
+        @test add!(u, cost)
+
+        @test buy!(u, ENERGYMIX)
+        @test u.buycount[:energymix] == i
+        @test has(u, cost) == false
+
+        @test getitem(u, ENERGYMIX) == i*ENERGYMIX
+    end
+
+end
+
+@testset "SiteCleaner 구매" begin
+    u = User()
+    @time getitem(u, SITECLEANER) == zero(SITECLEANER)
+
+    ref = get(DataFrame, ("SpaceDrop", "SiteCleaner"))
+    # TODO price가 제대로 계산됐는지 확인하는 함수 추가
+    for i in 1:50
+        cost = price(u, SITECLEANER)
+        @test buy!(u, SITECLEANER) == false
+        @test add!(u, cost)
+
+        @test buy!(u, SITECLEANER)
+        @test u.buycount[:sitecleaner] == i
+        @test has(u, cost) == false
+
+        @test getitem(u, SITECLEANER) == i*SITECLEANER
+    end
 
 end
