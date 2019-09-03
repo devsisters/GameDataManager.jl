4e0f8c0bbd0a7b7948f07d5df263dc1e5cb1df1a
김용희 <yongheekim@devsisters.com>
2019-09-03 16:33:41 +0900

    Player.xlsx Validator 추가

--- src/datahandler/balancetable/xlsx/Player.jl
+++ src/datahandler/balancetable/xlsx/Player.jl
@@ -1,3 +1,17 @@
+function validator_Player(bt)
+    df = get(DataFrame, bt, "DevelopmentLevel")
+
+    for el in skipmissing(df[!, :NewBuildingKey])
+        Building.(el) # Key 없으면 assert
+    end
+
+    p = joinpath(GAMEENV["CollectionResources"], "VillageGradeIcons")
+    validate_file(p, df[!, :GradeIcon], ".png", "Icon이 존재하지 않습니다")
+    # TODO 여러 폴더 검사하는 기능 필요
+    # p = joinpath(GAMEENV["CollectionResources"], "ItemIcons")
+    # validate_file(p, vcat(df[!, :DisplayIcons]...), ".png", "Icon이 존재하지 않습니다")
+
+end
 function editor_Player!(jwb)
     jwb[:DevelopmentLevel] = merge(jwb[:DevelopmentLevel], jwb[:DroneDelivery], "Level")
     jwb[:DevelopmentLevel] = merge(jwb[:DevelopmentLevel], jwb[:PartTime], "Level")
