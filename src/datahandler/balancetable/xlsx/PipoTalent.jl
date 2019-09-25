8be5f9edf84cfb501ec841653301cc392d58b69c
김용희 <yongheekim@devsisters.com>
2019-09-25 17:06:44 +0900

    PipoTalent 레전드 대사 대응

--- src/datahandler/balancetable/xlsx/PipoTalent.jl
+++ src/datahandler/balancetable/xlsx/PipoTalent.jl
@@ -2,30 +2,39 @@
 function editor_PipoTalent!(jwb::JSONWorkbook)
     output_path = joinpath(GAMEENV["mars_repo"], "patch-data/Dialogue/PipoTalk")
 
-    intro = JSON.parsefile(joinpath(output_path, "_Introduction.json"); dicttype=OrderedDict)
-    accept = JSON.parsefile(joinpath(output_path, "_Accepted.json"); dicttype=OrderedDict)
-    deny = JSON.parsefile(joinpath(output_path, "_Denied.json"); dicttype=OrderedDict)
+    template = Dict(
+        "Introduction" => JSON.parsefile(joinpath(output_path, "_Introduction.json"); dicttype=OrderedDict),
+        "Accepted"     => JSON.parsefile(joinpath(output_path, "_Accepted.json"); dicttype=OrderedDict),
+        "Denied"       => JSON.parsefile(joinpath(output_path, "_Denied.json"); dicttype=OrderedDict))
 
     jws = jwb["Dialogue"]
     println("$(output_path) Perk별 Dialogue가 생성됩니다")
+
     for el in jws.data
         perk = el["Key"]
-        intro[1]["\$Text"] = el["Introduction"]
-        accept[1]["\$Text"] = el["Accepted"]
-        deny[1]["\$Text"] = el["Denied"]
+        # print(" $(perk)가 말하고 있습니다.../")
+        for unique_grade in ("Normal", "Legend")
 
-        open(joinpath(output_path, "$(perk)Introduction.json"), "w") do io
-            JSON.print(io, intro, 2)
+            prefix = "$(perk)$(unique_grade)"
+
+            for f in ("Introduction", "Accepted", "Denied")
+                template[f][1]["\$Text"] = el[unique_grade]["Introduction"]
+                template[f][1]["\$Text"] = el[unique_grade]["Accepted"]
+                template[f][1]["\$Text"] = el[unique_grade]["Denied"]
+                
+                json = joinpath(output_path, "$(prefix)$(f).json")
+                newdata = JSON.json(template[f], 2)
+
+                modified = !isequal(md5(read(json, String)), md5(newdata))
+                # if modified
+                    write(json, newdata)
+                    # print(" SAVE => ")
+                    printstyled("$(prefix)$(f)... / "; color=:blue)
+                # end
+            end
         end
-        open(joinpath(output_path, "$(perk)Accepted.json"), "w") do io
-            JSON.print(io, accept, 2)
-        end
-        open(joinpath(output_path, "$(perk)Denied.json"), "w") do io
-            JSON.print(io, deny, 2)
-        end
-        print(" $(perk).../")
     end
-    printstyled(" ALL $(size(jws, 1)) PERK DONE!\n"; color=:cyan)
+    printstyled("\n$(size(jws, 1))개 PERK 대사 생성 완료!\n"; color=:cyan)
 
     deleteat!(jwb, "Dialogue")
     jwb
