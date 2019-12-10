f2c098c27b19f7d4d353d3122d2014b6badd9a24
김용희 <yongheekim@devsisters.com>
2019-12-10 11:46:54 +0900

    Dialogue Validation 추가

--- sub/GBM/src/dialogue.jl
+++ sub/GBM/src/dialogue.jl
@@ -16,6 +16,7 @@ function create_dialogue_script(data::AbstractArray, filename)
         el["CallOnStart"] = collect_values(el["CallOnStart"])
         el["CallOnEnd"] = collect_values(el["CallOnEnd"])
     end
+    validate_dialogue_script(data)
     newdata = JSON.json(data, 2)
 
     modified = true
@@ -29,4 +30,19 @@ function create_dialogue_script(data::AbstractArray, filename)
         printstyled(normpath(filename), "\n"; color=:blue)
     end
 
+end
+
+function validate_dialogue_script(data)
+    # TODO 지금 하드코딩인데 Dialogue.xlsx UIType 시트꺼 참고하기
+    types = ["Normal","Info","Input","Empty"]
+    
+    for row in data
+        x = get(row, "Type", missing)
+        if isnull(x)
+            throw(AssertionError("'Type'을 입력해주세요. Type은 $types 중 1개 사용할 수 있습니다"))
+        elseif !in(x, types)
+            @warn "Type에 \"$x\"는 사용할 수 없습니다 오탈자를 확인해 주세요" 
+        end
+    end
+    data
 end
