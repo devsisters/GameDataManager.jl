b6d0846ae6ef4b68930847fb1ad4caa94b67875f
김용희 <yongheekim@devsisters.com>
2020-02-20 20:31:22 +0900

    기본 테스트 추가

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,18 +5,22 @@ using GameDataManager
 if haskey(ENV, "GITHUB_WORKSPACE")
     GameDataManager.init_forCI("$(ENV["GITHUB_WORKSPACE"])/patch-data")
 end
-
-@test isfile(GameDataManager.joinpath_gamedata("_Meta.json"))
-
-
+@testset "테스트 환경 확인" begin 
+    @test isfile(GameDataManager.joinpath_gamedata("_Meta.json"))
+    println.(ENV)
+end
 # include("table.jl")
-@testset "JSON 컨버팅" begin 
+@testset "XLSX -> JSON 테스트 without validation" begin 
     files = GameDataManager.collect_auto_xlsx()
 
-    xl(files[1])
+    for f in files
+        data = Table(f;readfrom = :XLSX)
+        @test isa(f, GameDataManager.XLSXTable)
+    end
 end
 
 @testset "xlookup 기능" begin 
-
+    @test xlookup("Coin", Table("ItemTable")["Currency"], j"/Key", j"/$Name") == "코인"
+    @test xlookup("sIcecream", Table("Shop")["Building"], j"/BuildingKey", j"/$Name") == "아이스크림 가게"
 
 end
