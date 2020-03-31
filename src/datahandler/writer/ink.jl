266598535f6a8c8f843bf3291fa27df74b24afc5
김용희 <yongheekim@devsisters.com>
2020-03-31 09:23:46 +0900

    ink 파일 경로 변경사항 반영

--- src/datahandler/writer/ink.jl
+++ src/datahandler/writer/ink.jl
@@ -1,12 +1,12 @@
 # TODO 폴더 단위로 export 히스토리 관리하며 작업
-function ink(source = GAMEENV["Dialogue"])
+function ink(source = GAMEENV["InkDialogue"])
     exe = joinpath(dirname(pathof(GameDataManager)), "../deps/ink/inklecate.exe")
 
     #TODO 변경된 파일만 뽑기
     targets = []
     for (root, dirs, files) in walkdir(source)
 
-        output_path = replace(root, GAMEENV["Dialogue"] => 
+        output_path = replace(root, source => 
         joinpath(GAMEENV["patch_data"], "Dialogue"))
 
         for f in filter(el -> endswith(el, ".ink"), files)
