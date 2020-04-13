c668b1683101b4f259c66fb7f7ddc7a407dd7465
김용희 <yongheekim@devsisters.com>
2020-04-13 11:57:55 +0900

    update ssh 사용 안하고 temp폴더로 하도록 수정

--- src/_startup.jl
+++ src/_startup.jl
@@ -5,25 +5,26 @@ function checkout_GameDataManager()
         joinpath(root, "공유 드라이브/프로젝트 MARS/PatchDataOrigin/.tools")
     end
 
-    f = joinpath(network_folder_tools(), ".tools/version.toml")
-    if !isfile(f)
-        @warn """$(v2) 경로를 찾을 수 없습니다.
-        아래의 페이지를 참고하여 네트워크 폴더 세팅을 해 주세요
-        https://www.notion.so/devsisters/ccb5824c48544ec28c077a1f39182f01
-        """
-    else
-        pkginfo = Pkg.TOML.parsefile(f)
+    for pkgname in ("GameItemBase", "GameBalanceManager", "GameDataManager")
+        dir = joinpath(network_folder_tools(), pkgname)
+        projecttoml = joinpath(dir, "Project.toml")
 
-        for pkgname in ("GameItemBase", "GameBalanceManager", "GameDataManager")
+        if !isfile(projecttoml)
+            @warn """$pkgname 경로를 찾을 수 없습니다.
+            아래의 페이지를 참고하여 네트워크 폴더 세팅을 해 주세요
+            https://www.notion.so/devsisters/ccb5824c48544ec28c077a1f39182f01
+            """
+        else 
             v1 = get(Pkg.installed(), pkgname, missing)
-            v2 = pkginfo[pkgname][1]
+            project = Pkg.TOML.parsefile(projecttoml)
+
+            v2 = project["version"]
             
             if v1 < v2 # Pkg 업데이트
-                Pkg.setprotocol!(domain = "github.com", protocol = "ssh")
-
-                # TODO 이거 나중에 update로 변경
-                url = pkginfo[pkgname][2]
-                Pkg.add(url)
+                target = joinpath(tempdir(), "$pkgname")
+                cp(dir, target; force=true)
+                sleep(0.9)
+                Pkg.add(target)
             end
         end
     end
