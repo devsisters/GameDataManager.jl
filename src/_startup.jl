843191aba5a02a1a0dfa4cf8c4b7c9ed775c26c1
김용희 <yongheekim@devsisters.com>
2020-04-13 11:14:21 +0900

    update 참조 경로 github로 변경

--- src/_startup.jl
+++ src/_startup.jl
@@ -4,30 +4,29 @@ function checkout_GameDataManager()
         root = Sys.iswindows() ? "G:/" : "/Volumes/GoogleDrive/"
         joinpath(root, "공유 드라이브/프로젝트 MARS/PatchDataOrigin/.tools")
     end
-    function checkout_mars_package(package_name)
-        v1 = get(Pkg.installed(), package_name, missing)
-        if !ismissing(v1)
-            dir = network_folder_tools()
-            v2 = joinpath(dir, "$package_name/Project.toml")
-    
-            if isfile(v2)
-                v2 = readlines(v2)[4]
-                if v1 < VersionNumber(chop(v2; head=11, tail=1))
-                    @info "최신 버전의 $package_name 발견 되었습니다 업데이트를 시작합니다"
-                    Pkg.update(package_name)
-                end
-            else
-                @warn """$(v2) 경로를 찾을 수 없습니다.
-                아래의 페이지를 참고하여 네트워크 폴더 세팅을 해 주세요
-                https://www.notion.so/devsisters/ccb5824c48544ec28c077a1f39182f01
-                """
+
+    f = joinpath(network_folder_tools(), ".tools/version.toml")
+    if !isfile(f)
+        @warn """$(v2) 경로를 찾을 수 없습니다.
+        아래의 페이지를 참고하여 네트워크 폴더 세팅을 해 주세요
+        https://www.notion.so/devsisters/ccb5824c48544ec28c077a1f39182f01
+        """
+    else
+        pkginfo = Pkg.TOML.parsefile(f)
+
+        for pkgname in ("GameItemBase", "GameBalanceManager", "GameDataManager")
+            v1 = get(Pkg.installed(), pkgname, missing)
+            v2 = pkginfo[pkgname][1]
+            
+            if v1 < v2 # Pkg 업데이트
+                Pkg.setprotocol!(domain = "github.com", protocol = "ssh")
+
+                # TODO 이거 나중에 update로 변경
+                url = pkginfo[pkgname][2]
+                Pkg.add(url)
             end
         end
     end
-
-    checkout_mars_package("GameItemBase")
-    checkout_mars_package("GameBalanceManager")
-    checkout_mars_package("GameDataManager")
 
     nothing
 end
